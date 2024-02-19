22b1ffaeb03eb90f266dfbcaa2c746aa0506b036
Jessie Odeh <jessieodeh08@gmail.com>
2024-02-19 22:06:35 +0100

    refactor: change command description

--- src/commands/images/avatar.ts
+++ src/commands/images/avatar.ts
@@ -1,8 +1,8 @@
 import { CommandInteraction, SlashCommandBuilder } from 'discord.js';
 
 export const data = new SlashCommandBuilder()
- .setName('pfp')
- .setDescription("View a user' avatar.")
+ .setName('avatar')
+ .setDescription("View a user's avatar.")
  .addUserOption(option => option.setName('user').setDescription('Specify the user you want to get the avatar from.').setRequired(false));
 export async function execute(interaction: CommandInteraction<any>) {
  await interaction.reply((interaction.options.getUser('user') ?? interaction.user).avatarURL() as string);
