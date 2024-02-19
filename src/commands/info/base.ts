c0b1fa986a2bd7420a55d70ad0c19031892f36c4
Jessie Odeh <jessieodeh08@gmail.com>
2024-02-19 22:05:15 +0100

    refactor: compact info commands

---
+++ src/commands/info/base.ts
@@ -0,0 +1,29 @@
+import { SlashCommandBuilder } from 'discord.js';
+import type { CommandInteraction } from 'discord.js';
+
+export const data = new SlashCommandBuilder()
+ .setName('info')
+ .setDescription('Provides general information related to the guild.')
+ .addSubcommand(cmd => cmd.setName('bot').setDescription('Get my runtime info.'))
+ .addSubcommand(cmd => cmd.setName('server').setDescription('Provides information about the server.'));
+
+export async function execute(interaction: CommandInteraction) {
+ const cmd_name = interaction.options.data[0].name;
+ switch (cmd_name) {
+  case 'bot':
+   {
+    await interaction.reply(
+     `
+>>> Bot runtime: Bun ${process.versions.bun}
+Environment: ${import.meta.env.NODE_ENV}
+Shell: ${import.meta.env.SHELL?.split('/').pop()}
+Platform: ${process.platform}
+    `
+    );
+   }
+   break;
+  case 'server': {
+   await interaction.reply(`This server is called "${interaction?.guild?.name}" and has ${interaction?.guild?.memberCount} members.`);
+  }
+ }
+}
